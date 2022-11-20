2f4986d88ffa796d51deec875192e4d7921027ab
XiscoSM <118688023+XiscoSM@users.noreply.github.com>
2022-11-20 20:08:40 +0100

    Create CkChannelName.cs

---
+++ Models/CkChannelName.cs
@@ -0,0 +1,17 @@
+namespace EWeLink.Api.Models
+{
+    public class CkChannelName
+    {
+        [JsonProperty("0")]
+        public string Ch0_Name { get; set; }
+
+        [JsonProperty("1")]
+        public string Ch1_Name { get; set; }
+
+        [JsonProperty("2")]
+        public string Ch2_Name { get; set; }
+
+        [JsonProperty("3")]
+        public string Ch3_Name { get; set; }
+    }
+}
