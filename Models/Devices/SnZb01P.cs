932b2acc3ff7ffca63483fc9462c481f639138f5
Nick Archibald <n_archibald@outlook.com>
2024-04-03 20:03:17 +1300

    Added missing registration for the SNZB01-P button

---
+++ Models/Devices/SnZb01P.cs
@@ -0,0 +1,9 @@
+﻿namespace EWeLink.Api.Models.Devices
+{
+    using EWeLink.Api.Models.Parameters;
+
+    [DeviceIdentifier(7000)]
+    public class SnZb01P : Device<SnZbButtonParameters>
+    {
+    }
+}
